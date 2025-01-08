81938c575b9f4cf44ced32c3088aa62d4c6b7049
gold <narnavg@gmail.com>
2025-01-08 22:50:43 +0200

    -U

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 
 # Initialize the Gemini model
-llm4 = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key='AIzaSyDvFvjpe4CVEzcgIZnb-Xb3maIRX-wXZ-U')
+llm4 = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key='AIzaSyDvFvjpe4CVEzcgIZnb-Xb3maIRX-wXZU')
 
 # Define your query
 text = "What is the capital of the USA?"
